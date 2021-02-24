f17ea24a5ed1d4b2d07b1db5d5bda1e016923ffc
Rockie Guo <rockie@kitmi.com.au>
2021-02-24 14:41:40 +1100

    refactor: change hasOwnProperty back to get 2x perf increase

--- src/object/remap.js
+++ src/object/remap.js
@@ -3,7 +3,7 @@ import _each from 'lodash/each';
 /**
  * Remap the keys of object elements in an array, like projection.
  * @param {*} object
- * @param {*} mapping - key to newKey or key to array[ newKey, valueMap ] for next level mapping
+ * @param {Object} mapping - key to newKey or key to array[ newKey, valueMap ] for next level mapping
  * @param {boolean} keepUnmapped - If true, will keep those not in mapping as its original key, otherwise filter out
  */
 function remap(object, mapping, keepUnmapped) {
@@ -11,7 +11,9 @@ function remap(object, mapping, keepUnmapped) {
 
     let newObj = {};
     _each(object, (v, k) => {
-        if (Object.prototype.hasOwnProperty.call(mapping, k)) {
+        /* eslint-disable no-prototype-builtins */
+        if (mapping.hasOwnProperty(k)) {
+        /* eslint-enable no-prototype-builtins */
             let nk = mapping[k];
             if (Array.isArray(nk)) {
                 newObj[nk[0]] = {
